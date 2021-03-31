1e8513a6d7e506bc53bddf477599d5483070b1a5
ricardo <ricklimadealmeida@gmail.com>
2021-03-31 18:25:10 -0300

    adjusting some color effects

--- color-flipper/hex.js
+++ color-flipper/hex.js
@@ -1,6 +1,7 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
 const btn = document.querySelector('#btn')
 const color = document.querySelector('.color')
+const title = document.querySelector('#title')
 
 function getRandomNumber() {
     return Math.floor(Math.random() * hex.length)
@@ -11,4 +12,5 @@ btn.addEventListener('click', () => {
     for (i = 0; i < 6; i++) hexColor += hex[getRandomNumber()]
     color.textContent = hexColor
     document.body.style.backgroundColor = hexColor
+    title.style.color = hexColor
 })
--- color-flipper/index.js
+++ color-flipper/index.js
@@ -1,6 +1,7 @@
 const colors = ['green', 'red', 'rgba(133, 122, 200)', '#f15025']
 const btn = document.querySelector('#btn')
 const color = document.querySelector('.color')
+const title = document.querySelector('#title')
 
 function getRandomNumber() {
     return Math.floor(Math.random() * colors.length)
@@ -11,4 +12,5 @@ btn.addEventListener('click', () => {
     const randomNumber = getRandomNumber()
     document.body.style.backgroundColor = colors[randomNumber]
     color.textContent = colors[randomNumber]
+    title.style.color = colors[randomNumber]
 })
