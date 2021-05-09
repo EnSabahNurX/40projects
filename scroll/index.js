1dd4a1a85bed024a4ac77609012e801dfe4a3da7
ricardo <ricklimadealmeida@gmail.com>
2021-05-09 17:58:18 -0300

    setting the toggle

--- scroll/index.js
+++ scroll/index.js
@@ -4,9 +4,18 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+const date = document.querySelector('#date')
+date.innerHTML = new Date().getFullYear()
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle')
+const linksContainer = document.querySelector('.links-container')
+const links = document.querySelector('.links')
 
+navToggle.addEventListener('click', () => {
+    linksContainer.classList.toggle("show-links")
+    const containerHeight = linksContainer.getBoundingClientRect()
+    console.log(containerHeight)
+})
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
