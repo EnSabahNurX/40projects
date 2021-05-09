e8ba5ad7595cdbc65d0774b4732e51f9aff12edb
ricardo <ricklimadealmeida@gmail.com>
2021-05-09 18:25:25 -0300

    applying smooth scroll

--- scroll/index.js
+++ scroll/index.js
@@ -13,10 +13,32 @@ const links = document.querySelector('.links')
 
 navToggle.addEventListener('click', () => {
     linksContainer.classList.toggle("show-links")
-    const containerHeight = linksContainer.getBoundingClientRect()
-    console.log(containerHeight)
+    const containerHeight = linksContainer.getBoundingClientRect().height
+    const linksHeight = links.getBoundingClientRect().height
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`
+    } else {
+        linksContainer.style.height = 0
+    }
 })
 // ********** fixed navbar ************
+const navbar = document.querySelector('#nav')
+const topLink = document.querySelector('.top-link')
 
+window.addEventListener("scroll", () => {
+    const scrollHeight = window.pageYOffset
+    const navHeight = navbar.getBoundingClientRect().height
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav')
+    } else {
+        navbar.classList.remove('fixed-nav')
+    }
+    // go to top
+    if (scrollHeight > 500) {
+        topLink.classList.add('show-link')
+    } else {
+        topLink.classList.remove('show-link')
+    }
+})
 // ********** smooth scroll ************
 // select links
